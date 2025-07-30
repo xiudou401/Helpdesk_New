0b75f87c78cce9210b04346e368933d4c8a846f9
Will D <diweizhi@gmail.com>
2025-07-30 14:42:22 +1000

    getTicket

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const getTickets = async () => {
@@ -15,11 +16,13 @@ export default async function ticketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
