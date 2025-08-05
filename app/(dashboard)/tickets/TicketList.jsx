2efddc164c6b3d9ccde7d6d4874790ba3c33d6ae
Will D <diweizhi@gmail.com>
2025-08-05 12:49:26 +1000

    auth dashboard structure

--- app/(dashboard)/tickets/TicketList.jsx
+++ app/(dashboard)/tickets/TicketList.jsx
@@ -19,20 +19,17 @@ export default async function ticketList() {
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
           <Link href={`/tickets/${ticket.id}`}>
-
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
               {ticket.priority} priority
             </div>
-
           </Link>
         </div>
-
       ))}
       {tickets.length === 0 && (
         <p className="text-center">There are no open tickets, yay!</p>
       )}
     </>
   );
-
+}
