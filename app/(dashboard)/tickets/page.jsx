20e548396bd673fb52d18aa47bd7cf4d6bd2e52f
Will D <diweizhi@gmail.com>
2025-08-05 22:14:04 +1000

    metadata

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import TicketList from './TicketList';
 
+export const metadata = {
+  title: 'Dojo Helpdesk | Tickets',
+};
+
 export default function Tickets() {
   return (
     <main>
