726eecfa6d5464fbe114d1999bc1d583aa70eabe
Will D <diweizhi@gmail.com>
2025-08-06 14:23:18 +1000

    login signup form

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+'use client';
+import AuthForm from '../AuthForm';
 
 export default function Login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <main>
-      <h2 className="text-center">Login</h2>
+      <h2 className="text-center">Log in</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+'use client';
+import AuthForm from '../AuthForm';
 
 export default function Signup() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
