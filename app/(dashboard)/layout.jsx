960f8a2102689c12864bedf13a196ec2a46b3f9a
Will D <diweizhi@gmail.com>
2025-08-07 11:16:50 +1000

    redirect is not login

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,7 +1,8 @@
 // app/(protected)/layout.jsx or wherever your layout is
-import { cookies, headers } from 'next/headers';
+import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import NavBar from '../components/NavBar';
+import { redirect } from 'next/navigation';
 
 const DashboardLayout = async ({ children }) => {
   const supabase = createServerComponentClient({
@@ -11,6 +12,10 @@ const DashboardLayout = async ({ children }) => {
   const {
     data: { session },
   } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect('/login');
+  }
 
   return (
     <>
